33adb6ba9229c4bccc2533076d2329fa0aefca8b
Tomas Vojtech <tomas.vojtech@gmail.com>
2017-03-12 22:09:09 +0100

    feat: better action creator implementation

--- calculator-core/src/actions/index.js
+++ calculator-core/src/actions/index.js
@@ -1,20 +1,11 @@
 import * as actionNames from './names'
 
-export const addStep = payload => ({
-  type: actionNames.ADD_INSTRUCTION,
-  payload
-})
+const actionCreator = (type, payload) => {type, payload}
 
-export const addApply = payload => ({
-  type: actionNames.ADD_APPLY,
-  payload
-})
+export const addStep = payload => actionCreator(actionNames.ADD_INSTRUCTION, payload)
 
-export const registerPlugin = payload => {
-  return {
-    type: actionNames.REGISTER_PLUGIN,
-    payload
-  }
-}
+export const addApply = payload =>  actionCreator(actionNames.ADD_APPLY, payload)
+
+export const registerPlugin = payload => actionCreator(actionNames.REGISTER_PLUGIN, payload)
 
 export const names = actionNames
